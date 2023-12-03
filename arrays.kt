f8619581c4150cf89112a0a94045aaed89c3da11
nicolashoeller <hoellr3.0@gmail.com>
2023-12-03 17:53:47 +0100

    Make arrays

--- arrays.kt
+++ arrays.kt
@@ -1,4 +1,8 @@
 fun main() {
     var myArray = arrayOf(1,2,3,4)
+    var mySecondArray = Array(4){i -> i}        //[0,1,2,3]
+    println(mySecondArray[3])
+    println(myArray[3])
+    myArray[3] = 5
     println(myArray[3])
 }
