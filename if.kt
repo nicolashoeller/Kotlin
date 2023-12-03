e887e8e2cd327f5f4d6c4c2357dd5d53d7f82764
nicolashoeller <hoellr3.0@gmail.com>
2023-12-03 18:00:11 +0100

    if

---
+++ if.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var num1: Int = 52
+    var num2: Int = 34
+
+    if (num1 < num2){
+        println("Num1 ist kleiner als Num2")
+    }
+    else{
+        println("Num1 ist größer als Num2")
+    }
+}
