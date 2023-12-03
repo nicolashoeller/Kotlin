3df978750e7ecc2a6da5c5ab237af7d08763bcb1
nicolashoeller <hoellr3.0@gmail.com>
2023-12-03 15:27:56 +0100

    Exp 1

---
+++ Übungen/1.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val experiment1: Float = 3.1234567f             //Max Nachkommazahlen Float
+    val experiment2: Double = 3.123456789012345     //Max Nachkommazahlen Double
+    println(experiment1)
+    println(experiment2)
+}
