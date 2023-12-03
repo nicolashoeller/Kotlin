9d2a52c8cec76afe5cced4899ba11054098c9f56
nicolashoeller <hoellr3.0@gmail.com>
2023-12-03 19:40:06 +0100

    Booleans

---
+++ booleans.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var wahreitswertT: Boolean = true
+    var wahreitswertF: Boolean = false
+    val num1: Int = 20
+    val num2: Int = 40
+
+    if (wahreitswertT){
+        println("Hello")
+    }
+    if (wahreitswertF){
+        println("false")
+    }
+
+    var boolNums: Boolean = num1 < num2
+
+    if (boolNums){
+        println("Num2 is bigger than num1")
+    }
+}
